10da0ee2ee7e026f47fb365c63eb75c4314fdb04
emhamza <imumairhamza@gmail.com>
2025-08-01 19:18:06 +0500

    Add the google gemeini ai

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 #this is my main entity agent file
+import os
+import getpass
+from dotenv import load_dotenv
+
+
+#helper function to set enviroment variables
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+#load the enviroment variable
+load_dotenv()
+_set_env("GOOGLE_API_KEY")
